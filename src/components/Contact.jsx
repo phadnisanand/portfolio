4804726f64f255ec74d2953a0366235ae2a68a8e
Anand <phadnisanandhyd@gmail.com>
2024-10-19 19:47:24 +0530

    portfolio commir

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,37 +2,18 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div
-    name="contact"
-    id="contact"
-    className="w-full h-screen bg-[#0a192f] text-gray-300"
-  >
-    <div className="flex flex-col justify-center items-center w-full h-full">
-      <div className=" py-16 rounded-md bg-cyan-800 flex flex-col justify-center items-center w-4/6">
-        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8 mb-4">
-          <div className="sm:text-right pb-8 pl-4">
-            <p className="text-4xl font-bold inline border-b-4 border-cyan-500">
-              Contact Me
-            </p>
-          </div>
-          <div></div>
+        <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
+			<div className='flex flex-col max-w-[600px] w-full'>
+				<div className='pb-8 flex flex-col justify-center w-full h-full items-center'>
+					<p className='text-4xl font-bold inline border-b-4 border-cyan-500 text-gray-300'>Contact</p>
+					<p className='text-gray-300 py-4'>Send me a message</p>
+                    <p className='text-gray-300 py-4'>Name- Anand Phadnis  </p>
+					<p className='text-gray-300 py-4'>Role- Technical Lead </p>
+					<p className='text-gray-300 py-4'>Mobile - +91 7600785824</p>
+					<p className='text-gray-300 py-4'>Email- phadnisanandhyd@gmail.com</p>
+				</div>
         </div>
-        <div className="max-w-[1000px] w-full grid">
-          <div className="sm:text-center text-4xl font-bold">
-            <p> Anand Phadnis  </p>
-            <p>Technical Lead </p>
-            <p> +91 7600785824</p>
-            <p>phadnisanandhyd@gmail.com</p>
-          </div>
-          <div>
-            
-          </div>
-        </div>
-      </div>
     </div>
-  </div>
-
-
   )
 }
 export default Contact
