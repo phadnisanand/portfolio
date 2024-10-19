6a465f577ee26af2371635cbdf79947c8575e4af
Anand <phadnisanandhyd@gmail.com>
2024-10-19 19:51:22 +0530

    portfolio commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
         <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
 			<div className='flex flex-col max-w-[600px] w-full'>
-				<div className='pb-8 flex flex-col justify-center w-full h-full items-center'>
+				<div className=' py-16 rounded-md bg-cyan-800 flex flex-col justify-center items-center w-6/6'>
 					<p className='text-4xl font-bold inline border-b-4 border-cyan-500 text-gray-300'>Contact</p>
 					<p className='text-gray-300 py-4'>Send me a message</p>
                     <p className='text-gray-300 py-4'>Name- Anand Phadnis  </p>
